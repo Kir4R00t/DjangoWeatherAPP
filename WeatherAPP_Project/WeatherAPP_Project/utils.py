a1500b2c231073ef364cc9a64b5f066bbb60a305
Kir4R00t <przemyslaw.sadowski2004@gmail.com>
2024-04-14 15:44:25 +0200

    small changes for handling API & css changes

--- WeatherAPP_Project/WeatherAPP_Project/utils.py
+++ WeatherAPP_Project/WeatherAPP_Project/utils.py
@@ -4,21 +4,25 @@ import requests
 
 
 def get_weather_data(city_name):
+    
+    # TODO: fix api key not loading in
     load_dotenv('.env')
-    API_KEY = os.getenv('TOKEN')
+    API_KEY = os.getenv('WEATHER')
+    print(f"api key: {API_KEY}")
+
     URL = "https://api.openweathermap.org/data/2.5/weather?"
     request_url = URL + f"q={city_name}&units=metric&appid={API_KEY}"
-
-    # debugging
-    print(API_KEY)
-    print("Request URL:", request_url)
     response = requests.get(request_url)
+
+    # debugging printz
+    print("Request URL:", request_url)
     print("Response Status Code:", response.status_code)
 
     if response.status_code == 200:
         data = response.json()
         temperature = data['main']['temp']
         humidity = data['main']['humidity']
+        
         # Check if 'wind' data is available in the response
         if 'wind' in data:
             wind_speed = data['wind']['speed']
